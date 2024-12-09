c961c2a356aaf356540be6ae02582b72e97b12f9
lashu333 <140723330+lashu333@users.noreply.github.com>
2024-12-09 16:10:18 +0400

    Initial Commit

---
+++ notesApp13/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  notesApp13
+//
+//  Created by Lasha Tavberidze on 09.12.24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
